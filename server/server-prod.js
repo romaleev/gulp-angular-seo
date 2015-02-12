8d11ce4b35ce706b9252544b7382729fba69d9bb
Roman Malieiev <aromaleev@gmail.com>
2015-02-12 20:11:07 +0100

    font access control origin fix

--- server/server-prod.js
+++ server/server-prod.js
@@ -10,12 +10,13 @@ var express = require('express'),
 app.set('views', clientPath);
 app.use(require('compression')());
 
-app.use('/', function(req, res){//app.use(express.static(clientPath));
+app.use('/', function(req, res){
     if(/_escaped_fragment_=/.test(req.url)){//?_escaped_fragment_=
         req.url = req.url.replace(/\?.*$/,'').replace(/\/+$/,'');
         req.url += (req.url === '') ? '/index.html' : '.html';
         express.static(clientPath + '/snapshots').apply(this, arguments);
     } else {
+        if(/.(woff|woff2)/.test(req.url)) res.setHeader('Access-Control-Allow-Origin', '*');
     	express.static(clientPath).apply(this, arguments);
     }
 });
